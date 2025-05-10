254db0a44879da24a78ee8ca1c50bef41b1acbde
Arighna Chakraborty <riju23chakra@gmail.com>
2025-05-10 17:08:00 +0530

    initial tauri setup

--- src/preload/index.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-const ipcAPI = {
-    exampleFunc: (ping: boolean) => ipcRenderer.send("example-func", ping),
-};
-
-contextBridge.exposeInMainWorld("electronAPI", { ...ipcAPI });
